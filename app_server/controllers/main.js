2d62b1960ec0fc71ac777f7500dadb66173862fd
Alex Leet <alex@alexleet.com>
2025-07-18 15:26:06 -0400

    Updated Module 2

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -8,15 +8,10 @@
  * Course: CS 465 - Full Stack Development I
  */
 
-// This function runs when someone visits the homepage ("/")
-module.exports.index = function(req, res) {
-  // Show the index.hbs page and send it the info below
-  res.render('index', {
-    title: 'Travlr Getaways', // The title that shows in the browser tab
-    pageHeader: {
-      title: 'Travlr Getaways', // Big heading at the top of the page
-      strapline: 'Discover your next adventure!' // Small line under the heading
-    },
-    content: 'Welcome to Travlr — your personalized travel guide.' // Main text on the page
-  });
-};
+const index = (req, res) => {
+  res.render('index', { title: "Travlr Getaways"});
+}
+
+module.exports = {
+  index
+}
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -8,15 +8,8 @@
  * Course: CS 465 - Full Stack Development I
  */
 
-// This function runs when someone visits the "/travel" page
 module.exports.travel = function(req, res) {
-  // Show the travel.hbs page and send it the info below
   res.render('travel', {
-    title: 'Travlr | Travel Page', // Title shown in the browser tab
-    pageHeader: {
-      title: 'Our Travel Packages', // Main heading at the top of the page
-      strapline: 'Explore destinations around the world' // Small line under the heading
-    },
-    content: 'Browse a variety of travel options and find your next getaway!' // Main text shown on the page
+    title: 'Dive Sites - Bhaccasyoniztas Beach Resort Website Template'
   });
 };
