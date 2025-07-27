74e29f72ee525da0bc46b13fd89a0b2d34c43879
Alex Leet <alex@alexleet.com>
2025-07-27 12:15:30 -0400

    No changes

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -58,20 +58,23 @@ const gracefulShutdown = (msg) => {
 
 // Shutdown invoked by nodemon signal
 process.once('SIGUSR2', () => {
-    gracefulShutdown('nodemon restart');
+  gracefulShutdown('nodemon restart', () => {
     process.kill(process.pid, 'SIGUSR2');
+  });
 });
 
 // Shutdown invoked by app termination
 process.on('SIGINT', () => {
-    gracefulShutdown('app termination');
+  gracefulShutdown('app termination', () => {
     process.exit(0);
+  });
 });
 
 // Shutdown invoked by container termination
 process.on('SIGTERM', () => {
-    gracefulShutdown('app shutdown');
+  gracefulShutdown('app shutdown', () => {
     process.exit(0);
+  });
 });
 
 // Make initial connection to DB
